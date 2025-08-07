1e885550054a25ac73271c57ed028c98bab6020f
Denito37 <69406178+Denito37@users.noreply.github.com>
2025-08-07 18:30:48 -0400

    setting up frontend of extenstion

---
+++ sidebar/components/addGoal.tsx
@@ -0,0 +1,7 @@
+export default function addGoal(){
+    return(
+        <button>
+            +
+        </button>
+    )
+}
---
+++ sidebar/sidebar.tsx
@@ -0,0 +1,7 @@
+export default function Sidebar() {
+    return(
+        <div className="sidebar">
+            <h2>Goal Tracker</h2>
+        </div>
+    )
+}
